922e9a65acecf7e03e14fb52302c502cb0b4d4c4
RBD04 <rd1504dweik@gmail.com>
2023-05-07 00:08:09 +0300

    Closed a php tag

--- connection.php
+++ connection.php
@@ -7,3 +7,4 @@ $con = mysqli_connect($db_hostname, $db_username, $db_password, $db_database);
 if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
+?>
