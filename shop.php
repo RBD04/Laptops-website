b1415273a727790f592d0ba840d01e188fa6698e
RBD04 <rd1504dweik@gmail.com>
2023-05-06 19:41:59 +0300

    All Problems Fixed !

--- shop.php
+++ shop.php
@@ -58,9 +58,6 @@
                 <a class="nav-link" href="shop.php"> Shop <span class="sr-only">(current)</span></a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="about.php"> About </a>
-              </li>
-              <li class="nav-item">
                 <a class="nav-link" href="contact.php">Contact Us</a>
               </li>
             </ul>
