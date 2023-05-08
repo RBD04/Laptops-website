8042fbe11b44562159ed17a766bcef66689c4aab
RBD04 <rd1504dweik@gmail.com>
2023-05-08 19:47:48 +0300

    Login Page Style Done !(Its responsive now)

--- login.php
+++ login.php
@@ -41,9 +41,10 @@ if (isset($_POST) && isset($_POST['email']) && isset($_POST['password'])) {
   <meta name="author" content="" />
   <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
 
-  <title>FlOppy Shop</title>
+  <title>Laptops Website</title>
 
-
+ <!--Bootstrap 5.2 style link-->
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
   <!-- bootstrap core css -->
   <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
   <!--owl slider stylesheet -->
@@ -60,6 +61,8 @@ if (isset($_POST) && isset($_POST['email']) && isset($_POST['password'])) {
 </head>
 
 <body class="sub_page">
+  <!--Bootstrap 5.2 script section-->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
   <div class="hero_area">
 
@@ -80,13 +83,13 @@ if (isset($_POST) && isset($_POST['email']) && isset($_POST['password'])) {
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav">
               <li class="nav-item">
-                <a class="nav-link" href="index.php">Home </a>
+                <a class="nav-link fw-bolder" href="index.php">Home </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link text-primary" href="shop.php"> Shop </a>
+                <a class="nav-link fw-bolder" href="shop.php"> Shop </a>
               </li>
               <li class="nav-item active">
-                <a class="nav-link" href="contact.php">Contact Us <span class="sr-only">(current)</span> </a>
+                <a class="nav-link fw-bolder" href="contact.php">Contact Us <span class="sr-only">(current)</span> </a>
               </li>
             </ul>
             <div class="user_option-box">
@@ -109,13 +112,13 @@ if (isset($_POST) && isset($_POST['email']) && isset($_POST['password'])) {
 
   <!-- contact section -->
 
-  <section class="contact_section layout_padding pb-5">
-    <div class="container">
+  <section class="contact_section layout_padding">
+    <div class="container mt-2 px-5">
       <div class="form_container">
-        <div class="heading_container">
-          <h2>
+        <div class="text-center">
+          <h1 class="fw-bolder text-center text-primary">
             Login
-          </h2>
+          </h1>
         </div>
         <form method="post">
           <div>
@@ -124,13 +127,13 @@ if (isset($_POST) && isset($_POST['email']) && isset($_POST['password'])) {
           <div>
             <input name="password" type="password" placeholder="Password" />
           </div>
-          <div class="d-flex justify-content-center mb-4">
-            <button>
-              Sign in
-            </button>
-          </div>
+          <div class="text-center pb-3">
+            <a class="btn btn-large text-center btn-primary mx-auto">
+                &nbsp;Sign in &nbsp;
+            </a>
+            </div>
           <div class="d-flex justify-content-center">
-            <a href="register.php" style="text-decoration: underline; color:black;">Create Account</a>
+            <a href="register.php" class="text-secondary link">Create Account</a>
           </div>
         </form>
       </div>
@@ -140,7 +143,7 @@ if (isset($_POST) && isset($_POST['email']) && isset($_POST['password'])) {
   <!-- end contact section -->
 
   <!-- footer section -->
-  <footer class="footer_section bg-primary">
+  <footer class="footer_section bg-primary mb-0">
     <div class="container">
       <div class="row">
         <div class="col-md-6 col-lg-6 footer-col text-center">
@@ -151,7 +154,7 @@ if (isset($_POST) && isset($_POST['email']) && isset($_POST['password'])) {
             <p>
               Necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with
             </p>
-            <div class="footer_social">
+            <div class="footer_social justify-content-center">
               <a href="">
                 <i class="fa fa-facebook" aria-hidden="true"></i>
               </a>
@@ -197,11 +200,9 @@ if (isset($_POST) && isset($_POST['email']) && isset($_POST['password'])) {
       </div>
       <div class="footer-info">
         <p>
-          &copy; <span id="displayYear"></span> All Rights Reserved By
-          <a href="https://html.design/">FlOppy Shop</a>
+          &copy;<span id="displayYear"></span> All Rights Reserved By<a href="https://html.design/">&nbsp;Laptops website</a>
         </p>
       </div>
-    </div>
   </footer>
   <!-- footer section -->
 
